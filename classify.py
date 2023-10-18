cbae35284274f73bdf7ef95a8b0d589b0a080ea1
sorenpgithub <spaetau123@gmail.com>
2023-10-18 13:38:40 -0700

    Update classify.py

--- classify.py
+++ classify.py
@@ -10,7 +10,7 @@ def parser_check(filename):
     line1 = file.readline()
     line2 = file.readline()
     line3 = file.readline() #3 lines are special, check doc
-  if   
+  
   cols = line1.split(',')
   cols = [col.strip() for col in cols] #gets rid of white space and \n in col names
   for i in range(len(line2)):
@@ -26,12 +26,30 @@ def parser_check(filename):
 with open(sys.argv[2]) as json_file:
     tree = json.load(json_file)
 #initializing stuff
+#check if first element is leaf
 ret = parser_check(sys.argv[1])
 D = ret[0]
 ind_cols = [] #change
 
+def generate_preds(df_A, tree):
+  leaf = False
+  pred = []
+  for index, row in df_A.iterrows():
+    curr_node = tree["node"] #{"var":123: "edges":[....]}
+    A_i = curr_node["var"] 
+    obs_val = row[A_i] #value of observation in variable, A_i = # of bedroom \implies obs_val = 3
+    while not leaf:
+      for edg in curr_node["edges"]: #list of edges | edg = {"edge":{"value"}}
+        curr_edge = edg["edge"]
+        if curr_edge["value"] == obs_val: 
+          if "node" in curr_edge:
+            curr_node = curr_edge["node"] #scary since it changes list mid-iteration possibly bugs
+          else: #must be a leaf
+            pred_val = curr_edge["leaf"]["decision"]
+            pred.append(pred_val)
+            leaf = True
+          break 
+    return pred
 
-def prediction(row):
-  pass
 
-for
+  
