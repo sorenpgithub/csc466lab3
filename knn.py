94f2d63d5e81bb030b2db950b7b0d8788ba2aade
Othilia <74982948+Othilia@users.noreply.github.com>
2023-10-29 15:52:07 -0700

    Updatesss knn.py

--- knn.py
+++ knn.py
@@ -17,19 +17,19 @@ import classify
 """
 
 """
-def knn(D, class_var, k, categorical): #assuming D is encoded and that all cols \neq class are being used
+def knn(D, class_var, k, categ): #assuming D is encoded and that all cols \neq class are being used
     preds = []
-    
-    if len(categorical) != 0:
-        categorical.remove(class_var) #if categorical after this is empty --> only numerical (no dummification needed)
+
+    D_ = encode_df(D, categ, class_var)
+
+
     for i in range(D.shape[0]):
         dists = []
         ite = 0
-        D_wo_class = D.drop(class_var, axis = 1)
-        D_dum = pd.get_dummies(D_wo_class, columns = categorical).astype(int)
-        print("D_DUM: ", D_dum)
-        curr = D_dum.iloc[i].to_numpy()
-        matrix = D_dum.drop(i).to_numpy()
+        print(i)
+        
+        curr = D_.iloc[i].to_numpy()
+        matrix = D_.drop(i).to_numpy()
 
         for obs in matrix:
             dists.append(euclid(curr, obs))
@@ -58,10 +58,14 @@ def manhattan(n1, n2):
 def cosine(n1, n2):
     return np.dot(n1,n2) / (np.linalg.norm(n1)*np.linalg.norm(n2))
 
-def encode_df(D): #convert categ to numeric and normalize numeric!!!!!!!!!!!
-    dummies = pd.get_dummies(D)
-    print(dummies)
+def encode_df(D, categ, class_var): #convert categ to numeric and normalize numeric!!!!!!!!!!!
+    if len(categ) != 0:
+        categ.remove(class_var) #if categorical after this is empty --> only numerical (no dummification needed)
+    D_wo_class = D.drop(class_var, axis = 1)
+    D_dum = pd.get_dummies(D_wo_class, columns = categ)
+    D_dum = D_dum * 1 #makes sure that true and false are converted into 1 and 0
 #df = df.apply(pd.to_numeric, errors='coerce')
+    return D_dum
 
 def output_stuff(D, preds, correct, class_var):
   output = []
@@ -83,20 +87,17 @@ def output_stuff(D, preds, correct, class_var):
 def main():
     ret = classify.parser_check(sys.argv[1])
     D = ret[0] #need to decide how to encode THIS!!
-    categegorical = ret[2]
-    encode_df(D)
     class_var = ret[1]
+    categ = ret[2]
+    
     k = int(sys.argv[2]) #temp
 
-    preds = knn(D, class_var, k, categegorical)
+    preds = knn(D, class_var, k, categ)
     mask = [a == b for a, b in zip(preds, D[class_var])] #gross but should work
     count_correct = sum(mask)
     outs = output_stuff(D, preds, count_correct, class_var)
     for out in outs:
         sys.stdout.write(str(out) + "\n")
-
-
-
 
 
 if __name__ == "__main__":
