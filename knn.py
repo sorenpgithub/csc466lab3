7c644d4cfa1ee82b4acedc8c999db13e2dd846b5
Othilia <74982948+Othilia@users.noreply.github.com>
2023-10-29 11:39:46 -0700

    dummies knn.py

--- knn.py
+++ knn.py
@@ -1,3 +1,7 @@
+#CSC 466 Fall 2023 - Lab 3: Decision Trees, part 1
+#Othilia Norell and Soren Paetau \\ onorell@calpoly.edu  / spaetau@calpoly.edu
+
+#HOW TO RUN: 
 #knn.py dataset.csv K 
 import sys
 import pandas as pd
@@ -6,15 +10,22 @@ import json
 import classify
 
 
+"""
 
-def knn(D, class_var, k): #assuming D is encoded and that all cols \neq class are being used
+"""
+def knn(D, class_var, k, categorical): #assuming D is encoded and that all cols \neq class are being used
     preds = []
+    
+    categorical.remove(class_var)
     for i in range(D.shape[0]):
         dists = []
         ite = 0
-
-        curr = D.drop(class_var, axis = 1).iloc[i].to_numpy()
+        D_dum = pd.get_dummies(D, columns = categorical)
+        curr = D_dum.drop(class_var, axis = 1).iloc[i].to_numpy()
+        print("HEJ ", curr)
         matrix = D.drop(class_var, axis = 1).drop(i).to_numpy()
+
+        
 
         for obs in matrix:
             dists.append(euclid(curr, obs))
@@ -38,17 +49,20 @@ def euclid(n1, n2):
     return np.linalg.norm(n1 - n2) 
 
 def encode_df(D): #convert categ to numeric and normalize numeric!!!!!!!!!!!
-    pass
+    dummies = pd.get_dummies(D)
+    print(dummies)
 #df = df.apply(pd.to_numeric, errors='coerce')
 
 
 def main():
     ret = classify.parser_check(sys.argv[1])
     D = ret[0] #need to decide how to encode THIS!!
+    categegorical = ret[2]
+    encode_df(D)
     class_var = ret[1]
     k = int(sys.argv[2]) #temp
 
-    preds = knn(D, class_var, k)
+    preds = knn(D, class_var, k, categegorical)
     mask = [a == b for a, b in zip(preds, D[class_var])] #gross but should work
     count_correct = sum(mask)
     outs = classify.output_stuff(D, preds, count_correct, class_var)
